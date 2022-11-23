136bd080f7527276a1bb90b6446cd12b9018b8f9
LV <victor_barros@hotmail.com>
2022-11-23 18:59:18 -0300

    criando variaveis

---
+++ teste.js
@@ -0,0 +1,23 @@
+var nome
+var idade
+var peso
+var altura 
+var profissao
+
+nome = prompt("Insira a sua nome")
+idade = prompt("Insira seu idade")
+peso = prompt( "insira seu peso")
+altura = prompt("insira sua altura")
+profissao = prompt("informe sua profissao")
+
+//Para printar no console
+console.log(nome)
+console.log(idade)
+console.log(peso)
+console.log(altura)
+console.log("sua profissao é:"+ profissao) 
+
+console.log("ola seu nome é" + (nome),"voce tem"+(idade)+"anos", "voce pesa"+(peso),"sua altra é"+ (altura),"e sua profissao é"+(profissao) )
+ if(idade>18){console.log( "podebeber")}
+ if(idade<18){console.log( "naopodebeber")}
+
