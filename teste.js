ba4d3c15733260cdac05821471a4c32aaddb9922
LV <victor_barros@hotmail.com>
2022-11-23 20:52:07 -0300

    criando imc

--- teste.js
+++ teste.js
@@ -1,23 +1,42 @@
 var nome
 var idade
 var peso
-var altura 
+var altura
 var profissao
 
 nome = prompt("Insira a sua nome")
-idade = prompt("Insira seu idade")
-peso = prompt( "insira seu peso")
-altura = prompt("insira sua altura")
-profissao = prompt("informe sua profissao")
+idade = parseInt(prompt("Insira seu idade"))
+peso = parseFloat(prompt("insira seu peso"))
+altura = parseFloat(prompt("insira sua altura"))
+profissao = prompt("informe sua profissao")   
 
 //Para printar no console
 console.log(nome)
 console.log(idade)
 console.log(peso)
 console.log(altura)
-console.log("sua profissao é:"+ profissao) 
+console.log("sua profissao é:" + profissao)
 
-console.log("ola seu nome é" + (nome),"voce tem"+(idade)+"anos", "voce pesa"+(peso),"sua altra é"+ (altura),"e sua profissao é"+(profissao) )
- if(idade>18){console.log( "podebeber")}
- if(idade<18){console.log( "naopodebeber")}
+console.log("ola seu nome é" + (nome), "voce tem" + (idade) + "anos", "voce pesa" + (peso), "sua altra é" + (altura), "e sua profissao é" + (profissao))
+if (idade > 18) { console.log("podebeber") }
+if (idade < 18) { console.log("naopodebeber") }
+
+var imc = (peso / (altura * altura))
+
+if (imc <= 18.5){
+
+    console.log("seu imc é magreza")
+
+}else if (imc > 18.5 && imc <= 24.9){
+
+console.log("seu imc é normal")
+
+}else if (imc > 24.9 && imc <= 30){
+
+    console.log("seu imc é sobrepeso")
+}else{
+   
+    console.log("seu imc é de obesidade")
+    
+}
 
